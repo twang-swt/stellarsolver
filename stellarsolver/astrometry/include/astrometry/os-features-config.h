e80f63f4f4b9bf2b5cdf85e60d342aa16fd3ad2a
Tim <twang@skywatchertelescope.net>
2023-11-16 23:02:50 -0800

    Use fixed os-features-config.h
    - easier to cross compile

--- stellarsolver/astrometry/include/astrometry/os-features-config.h
+++ stellarsolver/astrometry/include/astrometry/os-features-config.h
@@ -1,4 +1,13 @@
-#define NEED_DECLARE_QSORT_R 0
-#define NEED_QSORT_R 1
-#define NEED_SWAP_QSORT_R 0
+#pragma once
+
+#if defined(_MSC_VER) || defined(__ANDROID__)
+    #define NEED_DECLARE_QSORT_R 1
+    #define NEED_QSORT_R 1
+    #define NEED_SWAP_QSORT_R 0
+#else
+    #define NEED_DECLARE_QSORT_R 0
+    #define NEED_QSORT_R 1
+    #define NEED_SWAP_QSORT_R 0
+#endif
+
 #define HAVE_NETPBM 0
