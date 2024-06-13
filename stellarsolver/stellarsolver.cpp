c3f3099f5a6d533a5b7f2ea83078aaab0e13c061
Tim <twang@skywatchertelescope.net>
2024-06-13 15:59:42 -0700

    Remove unnecessary library dependencies

--- stellarsolver/stellarsolver.cpp
+++ stellarsolver/stellarsolver.cpp
@@ -5,7 +5,6 @@
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
 */
-#include <QApplication>
 #include <QSettings>
 #if defined(__APPLE__)
 #include <sys/sysctl.h>
